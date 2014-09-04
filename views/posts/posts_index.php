b413852578545c8a0a67c22485321b40b6d28f70
AlphaSmack <mrtp1988@gmail.com>
2014-09-04 18:47:43 +0300

    Add posts listing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -5,7 +5,7 @@
         <?=$post['post_text']?>
     </p>
     <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label"><?=$post['user_id']?></span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     </div>
 </div>
 <?endforeach?>
