0a149a1f30c0ad9f85743c9bb1d9ef44f087809f
AlphaSmack <mrtp1988@gmail.com>
2014-09-04 19:17:33 +0300

    Add posts listing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,5 +1,4 @@
 <?php foreach($posts as $post) { ?>
-    <html>
     <div class="span8">
         <h1><?php echo $post['post_subject']; ?></h1>
         <p>
@@ -9,5 +8,4 @@
             <span class="badge badge-success"><?php echo $post['post_created']; ?></span><div class="pull-right"><span class="label"><?php echo $post['user_id']; ?></span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
         </div>
     </div>
-    </html>
 <?php } ?>
