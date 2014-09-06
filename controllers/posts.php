205b6340663808518308d598eedc6f2207e1a41a
AlphaSmack <mrtp1988@gmail.com>
2014-09-06 18:47:23 +0300

    Add posts listing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -2,6 +2,11 @@
 
 class posts extends Controller{
 
+    function view(){
+        $post_id = $this->params[0];
+        $this->post = get_first ("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+    }
+
 	function index(){
 		$this->posts = get_all("SELECT * FROM user");
 	}
