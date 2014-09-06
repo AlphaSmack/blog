1c2e8329bc0b686b8c99f39b8256586b22e031af
AlphaSmack <mrtp1988@gmail.com>
2014-09-06 19:21:03 +0300

    Add posts listing functionality

--- views/posts/posts_view.php
+++
@@ -1,9 +0,0 @@
-    <div class="span8">
-        <h1><?php echo $post['post_subject']; ?></h1>
-        <p>
-            <?php echo $post['post_text']; ?>
-        </p>
-        <div>
-            <span class="badge badge-success"><?php echo $post['post_created']; ?></span><div class="pull-right"><span class="label"><?php echo $post['user_id']; ?></span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
-        </div>
-    </div>
