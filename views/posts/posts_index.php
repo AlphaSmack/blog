9ad6c0f8273ba483ecd7720d8137b295f0435564
AlphaSmack <mrtp1988@gmail.com>
2014-09-08 13:27:15 +0300

    Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -11,7 +11,7 @@
     <div>
         <span class="badge badge-success">Posted <?=$post['post_created']?></span>
             <div class="pull-right">
-                <span class="label"><?=$post['user_id]?></span>
+                <span class="label"><?=$post['user_id']?></span>
                 <span class="label">story</span>
                 <span class="label">blog</span>
                 <span class="label">personal</span>
