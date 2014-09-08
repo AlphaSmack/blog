a20bed4b9bc5c6988139b40916b74ae352c83331
AlphaSmack <mrtp1988@gmail.com>
2014-09-08 11:04:43 +0300

    Rename welcome controller

--- controllers/posts.php
+++ controllers/posts.php
@@ -3,7 +3,7 @@
 class posts extends Controller{
 
 	function index(){
-		$this->users = get_all("SELECT * FROM user");
+		$this->posts = get_all("SELECT * FROM post");
 	}
 
     function index_ajax(){
@@ -15,4 +15,9 @@ class posts extends Controller{
 		echo "\$_POST:<br>";
 		var_dump($_POST);
 	}
+
+    function view(){
+        $post_id = $this ->params[0];
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+    }
 }
