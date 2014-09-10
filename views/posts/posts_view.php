3f2a27809f27d3a1022fa12057586fdb67d84280
AlphaSmack <mrtp1988@gmail.com>
2014-09-10 16:21:35 +0300

    Tags and comment start

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -6,8 +6,13 @@
     <div>
         <span class="badge badge-success">Posted <?=$post['post_created']?></span>
             <div class="pull-right">
-                <span class="label"><?=$post['user_id]?></span>
-                <span class="label">story</span>
+                <span class="label"><?=$post['user_id']?></span>
+                <span class="label">
+                    <?foreach ($tags as $tag):?>
+                        <a href="#"><span class="label label-info">
+                        <?=$tag['tag_name']?></span></a>
+                    <?endforeach?>
+                </span>
                 <span class="label">blog</span>
                 <span class="label">personal</span>
             </div>
