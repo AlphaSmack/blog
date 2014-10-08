862349561148f9e43da4fdd8af9c9c27745f1125
AlphaSmack <risto.mikelsaar@khk.ee>
2014-10-08 11:17:29 +0300

    update

--- doc/database.sql
+++ doc/database.sql
@@ -1,15 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 3.1.3.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Loomise aeg: Sept 15, 2014 kell 01:18 PM
--- Serveri versioon: 5.6.20
--- PHP versioon: 5.5.15
+-- Masin: localhost
+-- Tegemisaeg: 08.10.2014 kell 11:17:08
+-- Serveri versioon: 5.1.33
+-- PHP versioon: 5.2.9
 
 SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Andmebaas: `blog`
@@ -18,166 +18,103 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `comment`
+-- Struktuur tabelile `comment`
 --
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
-`com_id` int(10) unsigned NOT NULL,
-  `com_text` text NOT NULL,
-  `com_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `com_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `com_text` longtext NOT NULL,
+  `com_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author` varchar(20) NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
-  `post_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `post_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`com_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Andmete tõmmistamine tabelile `comment`
+-- Tabeli andmete salvestamine `comment`
 --
 
-INSERT INTO `comment` (`com_id`, `com_text`, `com_created`, `author`, `user_id`, `post_id`) VALUES
-(1, 'Esimene kommenntaar', '2014-09-15 11:00:48', 'testija', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `post`
+-- Struktuur tabelile `post`
 --
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
-`post_id` int(10) unsigned NOT NULL,
+  `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_subject` varchar(255) NOT NULL,
-  `post_text` text NOT NULL,
+  `post_text` longtext NOT NULL,
   `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Andmete tõmmistamine tabelile `post`
+-- Tabeli andmete salvestamine `post`
 --
 
-INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
-(1, 'tere1', 'post1', '2014-09-04 15:27:51', 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `post_tags`
+-- Struktuur tabelile `post_tags`
 --
 
 DROP TABLE IF EXISTS `post_tags`;
 CREATE TABLE IF NOT EXISTS `post_tags` (
-  `post_id` int(11) unsigned NOT NULL,
-  `tag_id` int(11) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `post_id` int(10) unsigned NOT NULL,
+  `tag_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`tag_id`,`post_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Andmete tõmmistamine tabelile `post_tags`
+-- Tabeli andmete salvestamine `post_tags`
 --
 
-INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
-(1, 1),
-(1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `tag`
+-- Struktuur tabelile `tag`
 --
 
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE IF NOT EXISTS `tag` (
-`tag_id` int(10) unsigned NOT NULL,
-  `tag_name` varchar(25) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Andmete tõmmistamine tabelile `tag`
+-- Tabeli andmete salvestamine `tag`
 --
 
-INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
-(1, 'najhaysc'),
-(2, 'casdcafa');
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `user`
+-- Struktuur tabelile `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Andmete tõmmistamine tabelile `user`
+-- Tabeli andmete salvestamine `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
-(1, 'demo', 'demo', 0);
 
---
--- Indeksid tõmmistatud tabelitele
---
-
---
--- Indeksid tabelile `comment`
---
-ALTER TABLE `comment`
- ADD PRIMARY KEY (`com_id`), ADD KEY `post_id` (`post_id`);
-
---
--- Indeksid tabelile `post`
---
-ALTER TABLE `post`
- ADD PRIMARY KEY (`post_id`), ADD KEY `user_id` (`user_id`);
-
---
--- Indeksid tabelile `post_tags`
---
-ALTER TABLE `post_tags`
- ADD PRIMARY KEY (`tag_id`,`post_id`), ADD KEY `post_id` (`post_id`);
-
---
--- Indeksid tabelile `tag`
---
-ALTER TABLE `tag`
- ADD PRIMARY KEY (`tag_id`);
-
---
--- Indeksid tabelile `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT tõmmistatud tabelitele
---
-
---
--- AUTO_INCREMENT tabelile `comment`
---
-ALTER TABLE `comment`
-MODIFY `com_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT tabelile `post`
---
-ALTER TABLE `post`
-MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT tabelile `tag`
---
-ALTER TABLE `tag`
-MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT tabelile `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- Tõmmistatud tabelite piirangud
+SET FOREIGN_KEY_CHECKS=1;
