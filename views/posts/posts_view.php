ff21c1f67d30bec2b8b50cf78ae69f78c362f1ac
AlphaSmack <risto.mikelsaar@khk.ee>
2014-10-08 13:32:54 +0300

    update

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -9,7 +9,8 @@
                 <span class="label"></span>
                 <span class="label">
                     <?foreach ($tags as $tag):?>
-                           <a href="tags/view/<?= $tag['tag_id'] ?>/<?= $tag['tag_name'] ?>"><span class="label label-info"><?=$tag['tag_name'] ?></span></a>
+                        <a href="#"><span class="label label-info">
+                        <?=$tag['tag_name']?></span></a>
 
                     <?endforeach?>
                 </span>
