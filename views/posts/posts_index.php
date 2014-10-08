1168ed4f899b345aa7e8575d4e7347cd5b47c2ba
AlphaSmack <risto.mikelsaar@khk.ee>
2014-10-08 13:24:04 +0300

    update

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,43 +1,32 @@
 <?foreach( $posts as $post ):?>
-<div class="span8">
-    <h1>
-        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>">
-            <?=$post['post_subject']?>
-        </a>
-    </h1>
-    <p>
-        <?=$post['post_text']?>
-    </p>
-    <div>
-        <span class="badge badge-success">Posted <?=$post['post_created']?> by <?=$post['username']?></span>
-        <div class="pull-right">
-            <span class="label"></span>
-            <? if(!empty($tags[$post['post_id']])): foreach ($tags[$post['post_id']] as $tag):?>
-                <a href="#">
-                    <span class="label" style="background-color: #5bc0de">
-                        <?=$tag?>
-                    </span>
-                </a>
-            <?endforeach; endif?>
-        </div>
+
+
+    <div class="background"></div>
+
+    <div id="post" class="col-xs-offset-1 col-xs-11 col-sm-offset-5 col-sm-6 col-md-offset-3 col-md-5">
+        <article id="post-1001" class="active">
+            <h1 class="title">
+                <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a>
+                <button type="button" class="close" aria-hidden="true">×</button>
+            </h1>
+            <div class="post">
+                <p>
+                    <?=$post['post_text']?>
+                </p>
+                <? if(!empty($tags[$post['post_id']])): foreach ($tags[$post['post_id']] as $tag):?>
+                    <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <?endforeach; endif?>
+            </div>
+        </article>
     </div>
-</div>
+
 <?endforeach?>
 
+    <hr/>
 <?php if ($auth->logged_in == true): ?>
-
-
-<form class="form-inline" method="post" role="form">
-    <div class="form-group">
-        <input class="form-control" type="text" placeholder="Pealkiri" name="data[post_subject]"/>
-
-    </div>
-    <br>
-    <textarea name="data[post_text]" style="width: 500px; height:150px; margin: 10px 0px 10px 0px; border-radius: 5px;" ></textarea>
-    <br>
-    <div class="form-group">
-        <button class="btn btn-default" type="submit">Add</button>
-    </div>
-</form>
-
+    <form method="post">
+        <input name="data[post_subject]" placeholder="Title" /><br><br>
+        <textarea name="data[post_text]" id="" cols="30" rows="5" placeholder="Content"></textarea>
+        <br><br>
+        <button class="btn btn-default">Post</button>
+    </form>
 <?php endif; ?>
