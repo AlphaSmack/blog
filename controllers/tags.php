be4942f1bc2c655bd6abf66c0b2abae2789e832c
AlphaSmack <mrtp1988@gmail.com>
2014-10-13 13:02:46 +0300

    Update tags.php
    update

--- controllers/tags.php
+++ controllers/tags.php
@@ -1,12 +1,20 @@
 <?php
 
-class tags extends Controller{
-
-    function index(){
-        $this->tags = get_all("SELECT tag_name, COUNT(post_id) AS count
-        FROM post_tags NATURAL JOIN tag GROUP BY tag_id");
+/**
+ * Created by PhpStorm.
+ * User: Maile
+ * Date: 4.09.14
+ * Time: 16:16
+ */
+class tags extends Controller
+{
+    function index()
+    {
+        $this->tags = get_all("SELECT tag_name, COUNT(post_id)AS count
+                            FROM post_tags NATURAL JOIN tag
+                            GROUP BY tag_id"
+        );
     }
-
     function index_ajax(){
         echo "\$_POST:<br>";
         var_dump($_POST);
@@ -16,7 +24,6 @@ class tags extends Controller{
         echo "\$_POST:<br>";
         var_dump($_POST);
     }
-
     function view()
     {
         $tag_name = $this->params[0];
