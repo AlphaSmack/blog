867a7d85be6749ee2782646010b39413d27282b3
AlphaSmack <mrtp1988@gmail.com>
2014-10-13 14:47:10 +0300

    Update posts_index.php

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,15 @@
 <?foreach( $posts as $post ):?>
-<div class="span8">
-    <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
-    <p><?=$post['post_text']?></p>
-    <div>
-        <?foreach ($tags[$post['post_id']] as $tag):?>
-            <a href="tags/view/<?= $tag['tag_id'] ?>/<?= $tag['tag_name'] ?>"><span class="label" style="background-color: #5bc0de"><?=$tag['tag_name']?></span></a> <? endforeach?>
+    <h1 class="title">
+        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a>
+        <button type="button" class="close" aria-hidden="true">×</button>
+    </h1>
+    <div class="post">
+        <p>
+            <?=$post['post_text']?>
+        </p>
+        <? if(!empty($tags[$post['post_id']])): foreach ($tags[$post['post_id']] as $tag):?>
+            <a href="tags/view/<?=$tag?>"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <?endforeach; endif?>
     </div>
-</div>
 <?endforeach?>
 </br>
  </hr>
