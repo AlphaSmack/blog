33d9a79f9f50a3c22fb4d3a900f1e03e6e35bed3
AlphaSmack <mrtp1988@gmail.com>
2014-10-13 13:07:03 +0300

    Update posts_index.php

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,25 +1,24 @@
 <?foreach( $posts as $post ):?>
-<div class="span8">
-    <h1>
-        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>">
-            <?=$post['post_subject']?>
-        </a>
+
+
+    <div class="background"></div>
+
+    <div id="post" class="col-xs-offset-1 col-xs-11 col-sm-offset-5 col-sm-6 col-md-offset-3 col-md-5">
+    <article id="post-1001" class="active">
+    <h1 class="title">
+        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a>
+        <button type="button" class="close" aria-hidden="true">×</button>
     </h1>
-    <p>
-        <?=$post['post_text']?>
-    </p>
-    <div>
-        <span class="badge badge-success">Posted <?=$post['post_created']?> by <?=$post['username']?></span>
-        <div class="pull-right">
-            <span class="label"></span>
-                <span class="label">
-                    <?foreach ($tags[$post['post_id']] as $tag):?>
-                        <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
-                    <? endforeach?>
-                </span>
-            </div>
+    <div class="post">
+        <p>
+            <?=$post['post_text']?>
+        </p>
+        <? if(!empty($tags[$post['post_id']])): foreach ($tags[$post['post_id']] as $tag):?>
+            <a href="tags/view/<?=$tag?>"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <?endforeach; endif?>
     </div>
+</article>
 </div>
+
 <?endforeach?>
 
 <?php if ($auth->logged_in == true): ?>
